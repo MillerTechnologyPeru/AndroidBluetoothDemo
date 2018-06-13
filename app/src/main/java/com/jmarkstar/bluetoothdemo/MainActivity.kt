b25c1c4a7f6a5b0e1b15309b03614d5b97dc9d00
jmarkstar <mestrella.1991@gmail.com>
2018-06-13 11:47:45 -0500

    Worked with `isDiscovering()`, `startDiscovery()` and `cancelDiscovery()` methods and `BluetoothDevice.ACTION_FOUND`, `BluetoothAdapter.ACTION_DISCOVERY_STARTED`, `BluetoothAdapter.ACTION_DISCOVERY_FINISHED`, `BluetoothDevice.EXTRA_DEVICE` and `BluetoothDevice.EXTRA_RSSI` constants.

--- app/src/main/java/com/jmarkstar/bluetoothdemo/MainActivity.kt
+++ app/src/main/java/com/jmarkstar/bluetoothdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jmarkstar.bluetoothdemo
 
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -21,14 +22,55 @@ class MainActivity : AppCompatActivity() {
 
     private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
+    private var isDiscovering = false
+
+    // BluetoothAdapter.ACTION_STATE_CHANGED
     private val blueoothChangeStateReceiver = object: BroadcastReceiver(){
 
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val state = intent?.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
-            val previousState = intent?.getIntExtra(BluetoothAdapter.EXTRA_PREVIOUS_STATE, -1)
+        override fun onReceive(context: Context?, intent: Intent) {
+            val state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
+            val previousState = intent.getIntExtra(BluetoothAdapter.EXTRA_PREVIOUS_STATE, -1)
 
             Log.v("BroadcastReceiver", "$previousState - $state")
-            handleStates(state!!)
+            handleStates(state)
+        }
+    }
+
+    // BluetoothAdapter.ACTION_DISCOVERY_STARTED
+    private val bluetoothDiscoveryStarted = object: BroadcastReceiver(){
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            tvDiscoveryMessage.text = "Discovery have started"
+            tvDiscoveryMessage.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.green))
+        }
+    }
+
+    // BluetoothAdapter.ACTION_DISCOVERY_FINISHED
+    private val bluetoothDiscoveryFinished = object: BroadcastReceiver(){
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            tvDiscoveryMessage.text = "Discovery have finished"
+            tvDiscoveryMessage.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.red))
+        }
+    }
+
+    // BluetoothDevice.ACTION_FOUND
+    private val bluetoothDiscoveredDevices = object: BroadcastReceiver(){
+
+        override fun onReceive(context: Context?, intent: Intent) {
+
+            // Get the bluetoothDevice object from the Intent
+            val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+
+            // Get the "RSSI" to get the signal strength as integer,
+            // but should be displayed in "dBm" units
+            val rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, java.lang.Short.MIN_VALUE)
+
+            Log.i("MainActivity", "Bluetooth Device: ${device.name}, ${device.address}, ${device.bondState}, ${device.type}, $rssi ")
+            if(device.uuids != null)
+                for(uuid in device.uuids){
+                    Log.i("MainActivity","Bluetooth Device: ${device.name} - uuid: $uuid")
+                }
         }
     }
 
@@ -37,32 +79,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnEnable.setOnClickListener {
-            if(bluetoothAdapter.isEnabled){
-                Toast.makeText(this, "Bluetooth is enabled", Toast.LENGTH_SHORT).show()
-            }else{
-                val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT)
-            }
+            isDiscovering = false
+            turnOnBluettothByUser()
         }
 
         handleStates()
+
         btnBluetoothState.setOnClickListener {
             when(bluetoothAdapter.state){
                 BluetoothAdapter.STATE_ON -> bluetoothAdapter.disable()
                 BluetoothAdapter.STATE_OFF -> bluetoothAdapter.enable()
             }
         }
+
+        btnStartDiscovery.setOnClickListener {
+            Log.i("MainActivity","starting discovery")
+            startDiscovery()
+        }
     }
 
     override fun onResume() {
         super.onResume()
+
+        //Enable
         val intentFilter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
         registerReceiver(blueoothChangeStateReceiver, intentFilter)
+
+        //Discovery
+        val intentDiscoverFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(bluetoothDiscoveredDevices, intentDiscoverFilter)
+
+        val intentDiscoveryStartedFilter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+        registerReceiver(bluetoothDiscoveryStarted, intentDiscoveryStartedFilter)
+
+        val intentDiscoveryFinishedFilter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        registerReceiver(bluetoothDiscoveryFinished, intentDiscoveryFinishedFilter)
     }
 
     override fun onPause() {
         super.onPause()
+
+        //Enable
         unregisterReceiver(blueoothChangeStateReceiver)
+
+        //Discovery
+        unregisterReceiver(bluetoothDiscoveredDevices)
+        unregisterReceiver(bluetoothDiscoveryStarted)
+        unregisterReceiver(bluetoothDiscoveryFinished)
+
+        if(bluetoothAdapter.isDiscovering)
+            bluetoothAdapter.cancelDiscovery()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -71,9 +137,29 @@ class MainActivity : AppCompatActivity() {
             if(resultCode == Activity.RESULT_OK){
                 Toast.makeText(this, "Bluetooth is allowed now", Toast.LENGTH_SHORT).show()
                 handleStates()
+
+                if(isDiscovering)
+                    startDiscovery()
             }else{
                 Toast.makeText(this, "Bluetooth was not allowed", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun startDiscovery(){
+        if(bluetoothAdapter.isDiscovering)
+            bluetoothAdapter.cancelDiscovery()
+        val state = bluetoothAdapter.startDiscovery()
+        if(!state){
+            isDiscovering = true
+            turnOnBluettothByUser()
+        }
+    }
+
+    private fun turnOnBluettothByUser(){
+        if(!bluetoothAdapter.isEnabled){
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT)
         }
     }
 
