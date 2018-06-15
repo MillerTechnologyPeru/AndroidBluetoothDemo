847d695acf9636e01ab030e66302dbbe7a9699e2
jmarkstar <mestrella.1991@gmail.com>
2018-06-14 23:50:30 -0500

    Added Gatt connection

--- ble/src/main/java/com/jmarkstar/demo/LeDeviceDetailActivity.kt
+++ ble/src/main/java/com/jmarkstar/demo/LeDeviceDetailActivity.kt
@@ -1,7 +1,10 @@
 package com.jmarkstar.demo
 
+import android.bluetooth.BluetoothGatt
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.jmarkstar.demo.le.DemoGattCallback
 import com.jmarkstar.demo.le.DemoLeDevice
 import com.jmarkstar.demo.le.DemoLeUtils
 import kotlinx.android.synthetic.main.activity_le_device_detail.*
@@ -12,21 +15,69 @@ class LeDeviceDetailActivity : AppCompatActivity() {
         const val DEVICE_PARAM = "intent_device_param"
     }
 
+    private var gattCallback: DemoGattCallback? = null
+    private var demoLeDevice: DemoLeDevice? = null
+
+    var isConnected = false
+    var gatt: BluetoothGatt? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_le_device_detail)
 
-        val demoLeDevice = intent.getParcelableExtra<DemoLeDevice>(DEVICE_PARAM)
+        gattCallback = DemoGattCallback(this)
 
-        tvDeviceName.text = String.format(getString(R.string.device_name), demoLeDevice.data.name ?: "No Name")
-        tvDeviceAddress.text = String.format(getString(R.string.device_address), demoLeDevice.data.address)
-        tvDeviceType.text = String.format(getString(R.string.device_type), DemoLeUtils.showDeviceType(demoLeDevice.data.type))
-        tvDeviceClassNumber.text = String.format(getString(R.string.device_class_number), demoLeDevice.data.bluetoothClass.deviceClass)
-        tvMajorDeviceClass.text = String.format(getString(R.string.major_device_class), DemoLeUtils.showDeviceClass(demoLeDevice.data.bluetoothClass.majorDeviceClass))
-        tvDeviceBondState.text = String.format(getString(R.string.bond_state), DemoLeUtils.showBondState(demoLeDevice.data.bondState))
+        demoLeDevice = intent.getParcelableExtra<DemoLeDevice>(DEVICE_PARAM)
+
+        tvDeviceName.text = String.format(getString(R.string.device_name), demoLeDevice?.data?.name ?: "No Name")
+        tvDeviceAddress.text = String.format(getString(R.string.device_address), demoLeDevice?.data?.address)
+        tvDeviceType.text = String.format(getString(R.string.device_type), DemoLeUtils.showDeviceType(demoLeDevice?.data?.type!!))
+        tvDeviceClassNumber.text = String.format(getString(R.string.device_class_number), demoLeDevice?.data?.bluetoothClass?.deviceClass)
+        tvMajorDeviceClass.text = String.format(getString(R.string.major_device_class), DemoLeUtils.showDeviceClass(demoLeDevice?.data?.bluetoothClass?.majorDeviceClass!!))
+        tvDeviceBondState.text = String.format(getString(R.string.bond_state), DemoLeUtils.showBondState(demoLeDevice?.data?.bondState!!))
 
         btnConnectGatt.setOnClickListener {
+            if(isConnected){
+                disconnectGatt()
+            } else {
+                connectGatt()
+            }
+        }
+    }
 
+    private fun connectGatt(){
+
+        demoLeDevice?.data?.connectGatt(this, false, gattCallback)
+
+        btnConnectGatt.visibility = View.GONE
+        pgConnecting.visibility = View.VISIBLE
+
+
+    }
+
+    private fun disconnectGatt(){
+        if(gatt!=null){
+            gatt?.disconnect()
+            gatt?.close()
+            isConnected = false
+        }
+    }
+
+    fun connected(){
+        btnConnectGatt.post {
+            btnConnectGatt.text = getString(R.string.gatt_disconnect)
+            btnConnectGatt.visibility = View.VISIBLE
+            pgConnecting.visibility = View.GONE
+            isConnected = true
+        }
+    }
+
+    fun disconnected(){
+        btnConnectGatt.post {
+            btnConnectGatt.text = getString(R.string.gatt_connect)
+            btnConnectGatt.visibility = View.VISIBLE
+            pgConnecting.visibility = View.GONE
+            isConnected = false
         }
     }
 }
--- ble/src/main/java/com/jmarkstar/demo/le/DemoGattCallback.kt
+++ ble/src/main/java/com/jmarkstar/demo/le/DemoGattCallback.kt
@@ -1,8 +1,27 @@
 package com.jmarkstar.demo.le
 
+import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothProfile
+import android.widget.Toast
+import com.jmarkstar.demo.LeDeviceDetailActivity
 
-class DemoGattCallback: BluetoothGattCallback() {
+class DemoGattCallback(private val activity: LeDeviceDetailActivity): BluetoothGattCallback() {
 
+    override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+        super.onConnectionStateChange(gatt, status, newState)
 
+        if(status != BluetoothGatt.GATT_SUCCESS){
+            Toast.makeText(activity, "Gatt connection error: $status", Toast.LENGTH_SHORT).show()
+            activity.disconnected()
+            return
+        }
+
+        if(newState == BluetoothProfile.STATE_CONNECTED){
+            activity.gatt = gatt
+            activity.connected()
+        } else if(newState == BluetoothProfile.STATE_DISCONNECTED){
+            activity.disconnected()
+        }
+    }
 }
